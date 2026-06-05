aafaf1f27804111f19f1e646386231e9623323b1
boypt <1033514+boypt@users.noreply.github.com>
2026-06-05 13:49:57 +0800

    refactor:  httpRequest function and serpAPISearchRequest method

--- api_handlers/base.lua
+++ api_handlers/base.lua
@@ -2,13 +2,17 @@ local logger = require("logger")
 local http = require("socket.http")
 local ltn12 = require("ltn12")
 local socket = require("socket")
+local socket_url = require("socket.url")
 local socketutil = require("socketutil")
 local https = require("ssl.https")
-local Device = require("device")
 local Trapper = require("ui/trapper")
-
+local json = require("rapidjson")
 local ffi = require("ffi")
 local ffiutil = require("ffi/util")
+local koutil = require("util")
+local T = ffiutil.template
+
+local assistant_utils = require("assistant_utils")
 
 local BaseHandler = {
     trap_widget = nil,  -- widget to trap the request
@@ -42,55 +46,73 @@ function BaseHandler:query(message_history, provider_setting, query_option)
     error("query method must be implemented")
 end
 
---- Post URL content with optional headers and body with timeout setting
---- code references: KOReader/frontend/ui/wikipedia.lua `getURLContent`
---- @param url any
---- @param headers any
---- @param body any
---- @param timeout any blocking timtout
---- @param maxtime any total response finished max time
---- @return boolean success, string status_code, string content
-local function postURLContent(url, headers, body, timeout, maxtime)
-    if string.sub(url, 1, 8) == "https://" then
-        https.cert_verify = false  -- disable CA verify
+local function httpRequest(url, timeout, maxtime, post_body, post_content_type, headers)
+    local parsed = socket_url.parse(url)
+    if parsed.scheme ~= "http" and parsed.scheme ~= "https" then
+        return false, "Unsupported protocol"
     end
+    if parsed.scheme == "https" then
+        https.cert_verify = false
+    end
+    if not timeout then timeout = 10 end
+    socketutil:set_timeout(timeout, maxtime or 30)
+
+    if not headers then headers = {} end
+    headers["Accept-Encoding"] = "gzip"
 
     local sink = {}
-    socketutil:set_timeout(timeout, maxtime)
     local request = {
-        url = url,
-        method = "POST",
-        headers = headers or {},
-        source = ltn12.source.string(body or ""),
-        sink = maxtime and socketutil.table_sink(sink) or ltn12.sink.table(sink),
+        url     = url,
+        method  = post_body and "POST" or "GET",
+        headers = headers,
+        sink    = maxtime and socketutil.table_sink(sink) or ltn12.sink.table(sink),
     }
-    local code, headers, status = socket.skip(1, http.request(request)) -- skip the first return value, not needed
-    socketutil:reset_timeout()
-    local content = table.concat(sink)  -- response body
+    if post_body then
+        local body = type(post_body) == "table" and socketutil.form_encode(post_body) or post_body
+        request.source = ltn12.source.string(body)
+        headers["Content-Type"]   = headers["Content-Type"] or post_content_type or "application/x-www-form-urlencoded"
+        headers["Content-Length"] = headers["Content-Length"] or tostring(#body)
+    end
 
-    -- check for timeouts
+    local code, resp_headers, status = socket.skip(1, http.request(request))
+    local content = table.concat(sink)
+    socketutil:reset_timeout()
+
     if code == socketutil.TIMEOUT_CODE or
        code == socketutil.SSL_HANDSHAKE_CODE or
-       code == socketutil.SINK_TIMEOUT_CODE then
-        logger.warn("request interrupted/timed out:", code)
-        return false, code, "Request interrupted/timed out"
+       code == socketutil.SINK_TIMEOUT_CODE
+    then
+        logger.warn("request interrupted:", code)
+        return false, code
     end
-
-    -- check for network errors
-    if headers == nil then
+    if resp_headers == nil then
         logger.warn("No HTTP headers:", status or code or "network unreachable")
-        return false, BaseHandler.CODE_NETWORK_ERROR, "Network Error: " .. status or code
+        return false, "Network or remote server unavailable" .. tostring(status or code)
+    end
+    if not code or code < 200 or code > 299 then
+        logger.warn("HTTP status not okay:", status or code or "network unreachable")
+        logger.dbg("Response headers:", resp_headers)
+        return false, "Remote server error or unavailable: " .. tostring(status or code)
     end
 
-    -- check response length
-    if headers and headers["content-length"] then
-        -- Check we really got the announced content size
-        local content_length = tonumber(headers["content-length"])
-        if #content ~= content_length then
-            return false, code, "Incomplete content received"
+    local http_len = assistant_utils.http_get_header(resp_headers, "content-length")
+    if http_len then
+        if #content ~= tonumber(http_len) then
+            return false, "Incomplete content received"
         end
     end
-    return true, code, content
+
+    if assistant_utils.http_is_encoded(resp_headers, "gzip") then
+        local decompressed, err = assistant_utils.zlib_uncompress_gzip(content, 8*1024*1024)
+        if not decompressed then
+            logger.warn("Failed to decompress data:", err)
+            return false, "Failed to decompress data:" .. tostring(err)
+        end
+        logger.info("content gzipped")
+        content = decompressed
+    end
+
+    return true, content
 end
 
 --- func description: Make a request to the specified URL with headers and body.
@@ -107,16 +129,19 @@ function BaseHandler:makeRequest(url, headers, body, timeout, maxtime)
             request_maxtime = maxtime or 120
         end
         -- If a trap widget is set, run the request in a subprocess
-        completed, success, code, content = Trapper:dismissableRunInSubprocess(function()
-                return postURLContent(url, headers, body, request_timeout, request_maxtime)
+        completed, success, content = Trapper:dismissableRunInSubprocess(function()
+                return httpRequest(url, request_timeout, request_maxtime, body, nil, headers)
             end, self.trap_widget)
         if not completed then
-            return false, self.CODE_CANCELLED, self.CODE_CANCELLED
+            return false, self.CODE_CANCELLED, content
         end
     else
         -- If no trap widget is set, run the request directly
         -- use smaller timeout because we are blocking the UI
-        success, code, content = postURLContent(url, headers, body, timeout or 20, maxtime or 45)
+        success, content = httpRequest(url, timeout or 20, maxtime or 45, body, nil, headers)
+    end
+    if not success then
+        code = self.CODE_NETWORK_ERROR
     end
 
     return success, code, content
@@ -167,6 +192,42 @@ function BaseHandler:backgroundRequest(url, headers, body)
         end
         ffi.C.close(child_write_fd)  -- close the write end of the pipe
     end
+end
+
+
+function BaseHandler:serpAPISearchRequest(serpconfig, keywords)
+
+    local base_url = serpconfig.serpapi_url or "https://serpapi.com/search"
+    local key = serpconfig.serpapi_key
+    local q = koutil.urlEncode(keywords)
+    local url = T("%1?engine=google_ai_mode&api_key=%2&q=%3", base_url, key, q)
+
+    local completed, success, content
+
+    local request_timeout = 45
+    local request_maxtime = 120
+
+    completed, success, content = Trapper:dismissableRunInSubprocess(function()
+            return httpRequest(url, request_timeout, request_maxtime, nil, nil, nil)
+        end, self.trap_widget)
+
+    if not completed then
+        return false, self.CODE_CANCELLED
+    end
+    if not success then
+        return false, content
+    end
+
+    local ok, parsed = pcall(json.decode, content)
+    if not ok or not parsed then
+        return false, "fail to parse serpapi return"
+    end
+
+    if parsed.reconstructed_markdown then
+        return true, parsed.reconstructed_markdown
+    end
+
+    return false, "Unrecognized SerpAPI result"
 end
 
 return BaseHandler
