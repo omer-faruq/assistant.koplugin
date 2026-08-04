a494c7bbe4ac6172f6d4ff9e2985b6b8be390da0
boypt <1033514+boypt@users.noreply.github.com>
2026-08-04 17:09:07 +0800

    version: bump to v1.14

--- _meta.lua
+++ _meta.lua
@@ -3,5 +3,5 @@ return {
     name = "assistant",
     fullname = _("AI Assistant"),
     description = _("💡 Enhance your KOReader experience with AI"),
-    version = "1.13",
+    version = "1.14",
 }
