f008918cdef08f6998dd3d26781343f446bef33d
boypt <1033514+boypt@users.noreply.github.com>
2026-09-21 17:16:01 +0800

    fix: close stale highlight menu after add/remove menu buttons

--- assistant_dialog.lua
+++ assistant_dialog.lua
@@ -501,6 +501,8 @@ function AssistantDialog:showAskDialog(highlightedText)
           ok_text = _("Add"),
           ok_callback = function()
             self.assistant:handleEvent(Event:new("AssistantSetButton", {order=quick_note_tab.order, idx=quick_note_tab.idx}, "add"))
+            -- Close the stale menu and clear the selection.
+            self.assistant.ui.highlight:onClose()
           end,
         })
       end,
@@ -641,6 +643,8 @@ function AssistantDialog:showAskDialog(highlightedText)
               ok_text = _("Add"),
               ok_callback = function()
                 self.assistant:handleEvent(Event:new("AssistantSetButton", {order=tab.order, idx=tab.idx}, "add"))
+                -- Close the stale menu and clear the selection.
+                self.assistant.ui.highlight:onClose()
               end,
             })
           end
--- main.lua
+++ main.lua
@@ -1173,6 +1173,8 @@ function Assistant:addMainButton(prompt_idx, prompt)
             ok_text = _("Remove"),
             ok_callback = function()
               self:handleEvent(Event:new("AssistantSetButton", {order=prompt.order, idx=prompt_idx}, "remove"))
+              -- Close the stale menu and clear the selection.
+              self.ui.highlight:onClose()
             end
           })
         end)
